022f56735dc251cd019650d3b78dac6cf0121c20
wahhabriaz <93725258+wahhabriaz@users.noreply.github.com>
2026-01-13 21:59:06 +0500

    Update Product.js

--- server/src/models/Product.js
+++ server/src/models/Product.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const productSchema = new mongoose.Schema(
   {
     title: { type: String, required: true, trim: true },
-    slug: { type: String, unique: true, index: true },
+slug: { type: String, unique: true, sparse: true, index: true },
 
     brand: { type: String, default: "" },
     category: { type: String, index: true, required: true },
@@ -27,5 +27,35 @@ const productSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+function slugify(str = "") {
+  return str
+    .toLowerCase()
+    .trim()
+    .replace(/[^a-z0-9]+/g, "-")
+    .replace(/(^-|-$)+/g, "");
+}
+
+productSchema.pre("validate", async function (next) {
+  if (!this.slug || this.slug === "null") {
+    this.slug = slugify(this.title);
+  }
+  next();
+});
+productSchema.pre("save", async function (next) {
+  if (!this.isNew) return next();
+
+  let base = this.slug;
+  let slug = base;
+  let i = 1;
+
+  const Product = mongoose.model("Product");
+  while (await Product.exists({ slug })) {
+    i += 1;
+    slug = `${base}-${i}`;
+  }
+  this.slug = slug;
+  next();
+});
+
 
 module.exports = mongoose.model("Product", productSchema);
