89e515d70e3ceb1a8d6ca07516daccc6685d9020
wahhabriaz <93725258+wahhabriaz@users.noreply.github.com>
2026-01-13 06:06:50 +0500

    Update store.js

--- src/App/store.js
+++ src/App/store.js
@@ -1,12 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartSlice from "../Features/Cart/cartSlice";
 import wishListSlice from "../Features/Wishlist/wishListSlice";
-
+import { apiSlice } from "../Features/api/apiSlice";
 const store = configureStore({
   reducer: {
     cart: cartSlice,
     wishlist: wishListSlice,
+    [apiSlice.reducerPath]: apiSlice.reducer,
   },
+   middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
 });
 
 export default store;
